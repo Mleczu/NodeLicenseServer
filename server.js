a0a63812da177de3cf5a24764247a3603fb2e129
Mleczuuu <mleczutokozak@gmail.com>
2023-09-08 15:47:04 +0200

    discord information on invalid license

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const rateLimit = require('express-rate-limit')
 const Database = require('./database')
 const logger = require('./logger')
 const { escape } = require('mysql')
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
 
 const db = Database.connect({ host: process.env.database_host, user: process.env.database_user, password: process.env.database_password, database: process.env.database_database })
 
 const app = express()
+const hook = new Webhook({
+    url: process.env.webhook_url,
+    throwErrors: false,
+    retryOnLimit: true
+})
+hook.setUsername("NodeLicenseServer")
+let antispam = {}
 
 const limiter = rateLimit({
 	windowMs: 10 * 1000,
@@ -26,7 +34,20 @@ app.get('/verify', async (req, res) => {
     if (!machine_id || !license_type) return res.status(400).json({ message: 'Bad request' })
     const data = await db("SELECT * FROM `licenses` WHERE `machine_id` = " + escape(machine_id) + " AND `license_type` = " + escape(license_type) + " AND `expires_at` > NOW() LIMIT 1")
     if (!data) return res.status(500).json({ message: "Server error" })
-    if (data.length == 0) return res.status(200).json({ access: false })
+    if (data.length == 0) {
+        if (antispam[machine_id] && antispam[machine_id] + (5 * 60 * 1000) > Date.now()) return res.status(200).json({ access: false })
+        if (!antispam[machine_id]) antispam[machine_id] = Date.now()
+        const embed = new MessageBuilder()
+            .setTitle("Unauthorized access")
+            .setAuthor(escape(machine_id).substring(1, escape(machine_id).length - 1))
+            .setColor("#eb4034")
+            .setTimestamp()
+            .setDescription("Tried to access **" + escape(license_type) + "** license, but it's not valid or expired.")
+            .setFooter("Requested from " + (req.ip || "Unknown IP address"))
+        hook.send(embed)
+
+        return res.status(200).json({ access: false })
+    }
     return res.status(200).json({ access: true, expires_at: data[0].expires_at })
 })
 
